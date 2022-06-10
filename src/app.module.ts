08230329b6b940fbd541b1748add787a766c6c1e
Richard Chou <richard@ezyraise.com>
2022-06-11 09:03:00 +1000

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'example',
+    database: 'nest-events'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
