567f8e4f66f7bfae38f370a8ce9e1bcaf1a358c1
Richard Chou <konami99@hotmail.com>
2022-06-19 13:18:27 +1000

    auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { AppDummy } from './app.dummy';
+import { AppJapanService } from './app.japan.service';
 import { AppService } from './app.service';
-import { Event } from './events/event.entity';
+import { AuthModule } from './auth/auth.module';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 import { EventsModule } from './events/events.module';
+import { SchoolModule } from './school/school.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'example',
-      database: 'nest-events',
-      entities: [Event],
-      synchronize: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true
     }),
-    EventsModule
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV === 'production' ? ormConfigProd : ormConfig
+    }),
+    AuthModule,
+    EventsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide: AppService,
+    useClass: AppJapanService
+  }, {
+    provide: 'APP_NAME',
+    useValue: 'Nest Events Backend'
+  }, {
+    provide: 'MESSAGE',
+    inject: [AppDummy],
+    useFactory: (app) => `${app.dummy()} Factory!`
+  }, AppDummy],
 })
 export class AppModule {}
