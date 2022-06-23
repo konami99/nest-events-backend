0f3a26e79b552b9c24a1fed549b8efb3ead7cc7d
Richard Chou <konami99@hotmail.com>
2022-06-23 20:02:17 +1000

    expose certain fields

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -7,28 +7,36 @@ import { Profile } from "./profile.entity";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
+  @Expose()
   id: number;
 
   @Column()
+  @Expose()
   username: string;
 
   @Column()
+  @Expose()
   password: string;
 
   @Column()
+  @Expose()
   email: string;
 
   @Column()
+  @Expose()
   firstName: string;
 
   @Column()
+  @Expose()
   lastName: string;
 
   @OneToOne(() => Profile)
   @JoinColumn()
+  @Expose()
   profile: Profile;
 
   @OneToMany(() => Event, (event) => event.organizer)
+  @Expose()
   organized: Event[];
 
   @OneToMany(() => Attendee, (attendee) => attendee.user)
--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { User } from "src/auth/user.entity";
 import { PaginationResult } from "src/pagination/paginator";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
@@ -5,35 +6,50 @@ import { Attendee } from "./attendee.entity";
 
 @Entity()
 export class Event {
+  constructor(partial?: Partial<Event>) {
+    Object.assign(this, partial);
+  }
+
   @PrimaryGeneratedColumn()
+  @Expose()
   id: number;
 
   @Column()
+  @Expose()
   name: string;
 
   @Column()
+  @Expose()
   description: string;
 
   @Column()
+  @Expose()
   when: Date;
 
   @Column()
+  @Expose()
   address: string;
 
   @OneToMany(() => Attendee, (attendee) => attendee.event, {
     cascade: true
   })
+  @Expose()
   attendees: Attendee[];
 
   @ManyToOne(() => User, (user) => user.organized)
+  @Expose()
   organizer: User;
 
   @Column({ nullable: true })
   organizerId: number;
 
+  @Expose()
   attendeeCount?: number;
+  @Expose()
   attendeeRejected?: number;
+  @Expose()
   attendeeMaybe?: number;
+  @Expose()
   attendeeAccepted?: number;
 }
 
